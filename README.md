5614cfcba78365b5cb3ad5ea1cd5c3233df8cda1
esteban <ecastro@educ.gob.ar>
2015-03-23 19:35:41 -0300

    Init

---
+++ README.md
@@ -0,0 +1,80 @@
+# Tiny JSON validator
+
+A Node.js lib that has *less lines than this readme™*.
+
+It supports a tiny subset of the [json schema](http://json-schema.org/) specs. The main goal of this project is
+to provide a nice and easy to follow implementation of a json validator. You may fork it and extend it as you please.
+
+# Installation
+
+```
+$ npm install tiny-json-validator
+```
+
+It is supported in all versions of [iojs](https://iojs.org) +1.4 without any flags.
+
+# Usage
+
+```javascript
+let validator = require('tiny-json-validator');
+
+
+let book_schema = {
+    type: 'object',
+    required: true,
+    properties: {
+        title: {
+            type: 'string',
+            required: true
+        },
+        author: {
+            type: 'object',
+            required: true
+            properties: {
+                name: {
+                    type: 'string',
+                    required: true
+                },
+                age: {
+                    type: 'integer',
+                    required: true
+                }
+                city: {
+                    type: 'string'
+                }
+            }
+        },
+        related_titles: {
+            type: 'array',
+            required: true,
+            items: {
+                type: 'string'
+            }
+        }
+    }
+};
+
+let data = {
+    title: 'A Game of Thrones',
+    author: {
+        name: 'George R. R. Marti'
+    },
+    related_titles: [1, 2],
+    extra: 'this will get removed'
+};
+
+res = validator(book_schema, data);
+res.isValid  // false
+res.errors  // {author.age: "is required", related_titles.0: 'type must be string', related_titles.1: 'type must be string'}
+data  // {title: 'A Game of Thrones', author: {name: 'George R. R. Marti'}, related_titles: [1, 2]}
+```
+
+# Running tests
+
+```
+$ jasmine
+```
+
+## License
+
+MIT
---
+++ spec/validator-spec.js
@@ -0,0 +1,211 @@
+"use strict";
+
+let validator = require('..');
+
+
+describe("Validator test suite", function() {
+
+    beforeEach(function() {
+        //...
+    });
+
+    afterEach(function() {
+        //...
+    });
+
+    // Types
+    it("handles object type", function() {
+        expect(validator({type: 'object', properties: {}}, {}).isValid).toBe(true);
+    });
+
+    it("handles nested object type", function() {
+        expect(validator({type: 'object', properties: {foo: {type: 'object', properties: {}}}}, {}).isValid).toBe(true);
+    });
+
+    it("handles array type", function() {
+        expect(validator({type: 'array', items: {type: 'integer'}}, []).isValid).toBe(true);
+    });
+
+    it("handles string type", function() {
+        expect(validator({type: 'string'}, 'foo').isValid).toBe(true);
+    });
+
+    it("handles integer type", function() {
+        expect(validator({type: 'integer'}, 1337).isValid).toBe(true);
+    });
+
+    it("throws on unknown type", function() {
+        expect(validator, {type: 'bad_type'}, {}).toThrow();
+    });
+
+    // Validations: is required
+    it("validates object is required", function() {
+        var schema = {type: 'object', properties: {foo: {type: 'string', required: true}}};
+        expect(validator(schema, {foo: 'what?'}).isValid).toBe(true);
+        expect(validator(schema, {}).errors).toEqual({foo: 'is required'});
+    });
+
+    it("validates nested object is required", function() {
+        var schema = {type: 'object', properties: {foo: {type: 'object', properties: {bar: {type: 'string', required: true}}}}};
+        expect(validator(schema, {foo: {bar: 'sup?'}}).isValid).toBe(true);
+        expect(validator(schema, {}).errors).toEqual({'foo.bar': 'is required'});
+    });
+
+    it("validates array is required", function() {
+        var schema = {type: 'array', required: true, items: {type: 'integer'}};
+        expect(validator(schema, [1337]).isValid).toBe(true);
+        expect(validator(schema, []).isValid).toBe(true);  // You are probably looking for minItems here, bad luck brian.
+    });
+
+    it("validates string is required", function() {
+        var schema = {type: 'string', required: true};
+        expect(validator(schema, '1337').isValid).toBe(true);
+        expect(validator(schema).errors).toEqual({ root: 'is required' });
+    });
+
+    it("validates integer is required", function() {
+        var schema = {type: 'integer', required: true};
+        expect(validator(schema, 1337).isValid).toBe(true);
+        expect(validator(schema).errors).toEqual({ root: 'is required' });
+    });
+
+    // Validations: type
+    it("validates object type", function() {
+        expect(validator({type: 'object', properties: {}}, []).errors).toEqual({ root: 'type must be object' });
+    });
+
+    it("validates nested object type", function() {
+        expect(validator({type: 'object', properties: {foo: {type: 'object', properties: {}}}}, {foo: []}).errors).toEqual({ foo: 'type must be object' });
+    });
+
+    it("validates array type", function() {
+        var schema = {type: 'array', items: {type: 'integer'}};
+        expect(validator(schema, '1337').errors).toEqual({root: 'type must be array' });
+        expect(validator(schema, ['1337']).errors).toEqual({0: 'type must be integer'});
+    });
+
+    it("validates string type", function() {
+        expect(validator({type: 'string'}, 1337).errors).toEqual({ root: 'type must be string' });
+    });
+
+    it("validates integer type", function() {
+        expect(validator({type: 'integer'}, '1337').errors).toEqual({ root: 'type must be integer' });
+    });
+
+    // Validations: format
+    it("validates date-time format", function() {
+        var schema = {type: 'string', format: 'date-time'};
+        expect(validator(schema, '2010-06-09T15:20:00Z').isValid).toBe(true);
+        expect(validator(schema, '(-_____- U)').errors).toEqual({ root: 'format must be date-time' });
+    });
+
+    it("validates date format", function() {
+        var schema = {type: 'string', format: 'date'};
+        expect(validator(schema, '2010-06-09').isValid).toBe(true);
+        expect(validator(schema, 'U_____U').errors).toEqual({ root: 'format must be date' });
+    });
+
+    it("validates time format", function() {
+        var schema = {type: 'string', format: 'time'};
+        expect(validator(schema, '15:20:00').isValid).toBe(true);
+        expect(validator(schema, '?____?').errors).toEqual({ root: 'format must be time' });
+    });
+
+    it("validates email format", function() {
+        var schema = {type: 'string', format: 'email'};
+        expect(validator(schema, 'geezchrist@gmail.com').isValid).toBe(true);
+        expect(validator(schema, 'T____T').errors).toEqual({ root: 'format must be email' });
+    });
+
+    it("validates ip-address format", function() {
+        var schema = {type: 'string', format: 'ip-address'};
+        expect(validator(schema, '11.111.111.111').isValid).toBe(true);
+        expect(validator(schema, '<____<').errors).toEqual({ root: 'format must be ip-address' });
+    });
+
+    it("validates ipv4 format", function() {
+        var schema = {type: 'string', format: 'ipv4'};
+        expect(validator(schema, '11.111.111.111').isValid).toBe(true);
+        expect(validator(schema, '>____>').errors).toEqual({ root: 'format must be ipv4' });
+    });
+
+    it("validates ipv6 format", function() {
+        var schema = {type: 'string', format: 'ipv6'};
+        expect(validator(schema, 'FE80:0000:0000:0000:0202:B3FF:FE1E:8329').isValid).toBe(true);
+        expect(validator(schema, 'o____O').errors).toEqual({ root: 'format must be ipv6' });
+    });
+
+    it("validates uri format", function() {
+        var schema = {type: 'string', format: 'uri'};
+        expect(validator(schema, 'localhost:1337').isValid).toBe(true);
+        expect(validator(schema, 'o___o').errors).toEqual({ root: 'format must be uri' });
+    });
+
+    it("validates color format", function() {
+        var schema = {type: 'string', format: 'color'};
+        expect(validator(schema, '#FFF').isValid).toBe(true);
+        expect(validator(schema, '#FFFFFF').isValid).toBe(true);
+        expect(validator(schema, 'white').isValid).toBe(true);
+        expect(validator(schema, 'D____D').errors).toEqual({ root: 'format must be color' });
+    });
+
+    it("validates hostname format", function() {
+        var schema = {type: 'string', format: 'hostname'};
+        expect(validator(schema, 'tedmosbyisajerk.com').isValid).toBe(true);
+        expect(validator(schema, 'himym?').errors).toEqual({ root: 'format must be hostname' });
+    });
+
+    it("validates alpha format", function() {
+        var schema = {type: 'string', format: 'alpha'};
+        expect(validator(schema, 'Marshall').isValid).toBe(true);
+        expect(validator(schema, 'Ted000').errors).toEqual({ root: 'format must be alpha' });
+    });
+
+    it("validates alphanumeric format", function() {
+        var schema = {type: 'string', format: 'alphanumeric'};
+        expect(validator(schema, 'Lily000').isValid).toBe(true);
+        expect(validator(schema, 'Robin?').errors).toEqual({ root: 'format must be alphanumeric' });
+    });
+
+    it("validates style format", function() {
+        var schema = {type: 'string', format: 'style'};
+        expect(validator(schema, 'display:none;').isValid).toBe(true);
+        expect(validator(schema, '???').errors).toEqual({ root: 'format must be style' });
+    });
+
+    it("validates phone format", function() {
+        var schema = {type: 'string', format: 'phone'};
+        expect(validator(schema, '+54123123123').isValid).toBe(true);
+        expect(validator(schema, '???').errors).toEqual({ root: 'format must be phone' });
+    });
+
+    it("validates utc-millisec format", function() {
+        var schema = {type: 'string', format: 'utc-millisec'};
+        expect(validator(schema, '123123123123').isValid).toBe(true);
+        expect(validator(schema, '???').errors).toEqual({ root: 'format must be utc-millisec' });
+    });
+
+    // Filtering
+    it("removes extra properties", function() {
+        var schema = {type: 'object', properties: {foo: {type: 'string'}}};
+        var data = {foo: '1337', bar: 123};
+        expect(validator(schema, data).isValid).toBe(true);
+        expect(data).toEqual({foo: '1337'});
+    });
+
+    it("removes nested extra properties", function() {
+        var schema = {type: 'object', properties: {foo: {type: 'object', properties: {bar: {type: 'string'}}}}};
+        var data = {foo: {bar: '1337', foo2: 123}, bar2: 123};
+        expect(validator(schema, data).isValid).toBe(true);
+        expect(data).toEqual({foo: {bar: '1337'}});
+    });
+
+    it("removes extra properties from array items", function() {
+        var schema = {type: 'array', items: {type: 'object', properties: {foo: {type: 'string'}}}};
+        var data = [{foo: '1337', bar: 123}, {bar: 123}];
+        expect(validator(schema, data).isValid).toBe(true);
+        expect(data).toEqual([{foo: '1337'}, {}]);
+    });
+
+});
+
